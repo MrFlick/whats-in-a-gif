80e57c2879e959cc7eaa29cfeb2e9e02779009b0
Matthew Flickinger <mflick@umich.edu>
2020-04-20 02:23:32 -0400

    Include simple dev server script for '.asp' pages

---
+++ devserver.py
@@ -0,0 +1,30 @@
+"""
+This is a silly little script to spin up 
+a dev webserver to test the pages properly.
+This will just serve the .asp pages as plain
+HTML pages.
+
+Run with 
+
+python devserver.py [port]
+"""
+import sys
+from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+SimpleHTTPRequestHandler.extensions_map.update({".asp": "text/html"})
+
+host = 'localhost'
+port = 8080
+if (len(sys.argv) == 2):
+    port = int(sys.argv[1])
+else:
+    port = 8080
+
+httpd = HTTPServer((host, port), SimpleHTTPRequestHandler)
+print(f"Serving HTTP on {host} port {port} ")
+
+try:
+    httpd.serve_forever()
+except KeyboardInterrupt:
+    print("\nKeyboard interrupt received, exiting.")
+    sys.exit(0)
